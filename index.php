cd3440a1db6a2338c6aa26e722805d265e972c31
Jarrey <jar_bob@163.com>
2026-05-05 21:59:36 +0200

    主界面播放器收藏按钮支持多语言 title

--- index.php
+++ index.php
@@ -396,7 +396,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'stations') {
                 </div>
             </div>
             <audio controls id="audioPlayer"></audio>
-            <button class="fullscreen-btn" id="playerFavBtn" title="收藏">
+            <button class="fullscreen-btn" id="playerFavBtn" data-i18n-title="favAdd" title="收藏">
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
             </button>
             <button class="fullscreen-btn" id="fullscreenBtn" data-i18n-title="fullscreen" title="全屏播放">
